f4fe14268dfb2bd324fb81b9a900fac1619842f0
petrashalla <168079826+petrashalla@users.noreply.github.com>
2024-05-14 22:12:07 +0300

    src_files

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+// Slider
+  // const swiper = new Swiper(".swiper", {
+  //   scrollbar: {
+  //     el: ".swiper-scrollbar",
+  //     hide: true,
+  //   },
+  // });
+
+
+  // var swiper = new Swiper(".swiper", {
+  //   slidesPerView: 3,
+  //   grid: {
+  //     rows: 2,
+  //   },
+  //   spaceBetween: 30,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  // });
+
+
+
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    //centeredSlides: true,
+    //freeMode: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      clickable: true,
+    },
+  });
